c13afec6284280f7b510d98f273e011ad6495772
Nicola Murino <nicola.murino@gmail.com>
2024-02-16 19:08:41 +0100

    remove metadata plugin
    Signed-off-by: Nicola Murino <nicola.murino@gmail.com>

--- plugin/mkproto.sh
+++ plugin/mkproto.sh
@@ -4,5 +4,4 @@ protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimpleme
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative kms/proto/kms.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative auth/proto/auth.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative eventsearcher/proto/search.proto
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative metadata/proto/metadata.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative ipfilter/proto/ipfilter.proto
